624c575ac81a9b6f2e24da2a756ec4753073f4d2
ArjunDahal99 <dahalarjun409@gmail.com>
2024-09-12 15:24:17 +0545

    error was corrected in models.py

--- om_python/core/models.py
+++ om_python/core/models.py
@@ -1,6 +1,6 @@
-from django.db import models
+'''from django.db import models
 
-# Create your models here.
+# Create your models here.p
 class Category(models.Model):
     name=models.CharField(max_length=31)
     description=models.TextField(
@@ -31,6 +31,40 @@ class Product(models.Model):
     languagfe=models.charfield(choices=LANGUAGE_CHOICCES,max_length=2)
     published_date=models.DataField()
 
-    class ProductCart(Models.Models)
+    class ProductCart(models.Models):
+        pass
 
+'''
+from django.db import models
 
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=31)
+    description = models.TextField(null=True, blank=True)
+
+class Author(models.Model):  # Fixed: Added colon (:) and corrected indentation
+    name = models.CharField(max_length=63)
+
+class Product(models.Model):
+    LANGUAGE_CHOICES = (
+        ("EN", "English"),
+        ("NP", "Nepali"),
+    )
+    name = models.CharField(max_length=127)
+    isbn_number = models.CharField(max_length=127, unique=True)
+    description = models.TextField(null=True, blank=True)  # Fixed: Changed Textfield to TextField
+    author = models.ForeignKey(
+        "Author", on_delete=models.PROTECT, related_name="products"
+    )
+    price = models.FloatField()
+    image = models.URLField(blank=True, null=True)  # Fixed: Corrected 'niull' to 'null'
+    category = models.ForeignKey(  # Fixed: Changed 'foreignKey' to 'ForeignKey'
+        "Category", related_name="products", on_delete=models.PROTECT  # Fixed: Changed 'PRODUCT' to 'PROTECT'
+    )
+    rating = models.FloatField(default=0)
+    page_count = models.IntegerField()
+    language = models.CharField(choices=LANGUAGE_CHOICES, max_length=2)  # Fixed: Corrected 'languagfe' and 'charfield' to 'CharField'
+    published_date = models.DateField()  # Fixed: Changed 'DataField' to 'DateField'
+
+class ProductCart(models.Model):  # Fixed: Changed 'Models' to 'Model'
+    pass
