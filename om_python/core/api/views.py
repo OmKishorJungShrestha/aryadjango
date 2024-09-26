f895f05b179523deca20c02caa53700c0ba1ed76
OmKishorJungShrestha <omshrestha40@gmail.com>
2024-09-26 16:07:00 +0545

    serializers.py created at core

--- om_python/core/api/views.py
+++ om_python/core/api/views.py
@@ -1,13 +1,19 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from core.models import Genre
+from core.api.serializers import GenreSerializer
 
 class BookListAPIView(APIView):
     def get(self, request, *args, **kwargs):
         # Create a list of books
-        response = [
-            {"name": "Think and Grow Rich", "author": "Napoleon Hill","products": Product } 
-            
-        ]
+        #products = Product.objects.all()
+        Genre = Genre.objects.all() # select * from genre
+        print(type(genre))
+        serializer= GenreSerializer(genre,many=true)
+        #response =[{"name":"Think and grow rich", "author":"Nepoview"}]
+        response={"genres": serializer.data}
+        return Response(response)
+        #response = [{"name": "Think and Grow Rich", "author": "Napoleon Hill","products": Product } ]
         # Return the response with the book data
         return Response(response)
     
